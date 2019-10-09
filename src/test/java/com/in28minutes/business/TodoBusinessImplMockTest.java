9e6cd69bb57bfee12bf93ed9bc09a7036a6fb568
Julian Cabrera <julian.cabreras@gmail.com>
2019-10-09 13:00:24 -0500

    Stub and Mocks

---
+++ src/test/java/com/in28minutes/business/TodoBusinessImplMockTest.java
@@ -0,0 +1,28 @@
+package com.in28minutes.business;
+
+import com.in28minutes.data.api.TodoService;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class TodoBusinessImplMockTest {
+
+    @Test
+    public void testRetrieveTodosRelatedToSpring_usingAMock(){
+        TodoService  todoServiceMock = mock(TodoService.class);
+        List<String> todos = Arrays.asList("Learng Spring MVC", "Learn Spring", "Learn to Dance");
+        when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
+
+        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+        List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
+        assertEquals(2, filteredTodos.size());
+
+    }
+
+
+}
